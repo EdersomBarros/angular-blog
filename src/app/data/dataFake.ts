7d882f788b94f38d6e4c3a974c091fa74c009204
Edersom Barros <edersombarros@hotmail.com>
2023-09-25 15:21:16 -0400

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,42 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'NOVO HOMEM DE FERRO ANUNCIADO',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    photoCover:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW3X2pZ-GOOXBiN3GIE28uPOKTDwTYsmekgg&usqp=CAU',
+  },
+  {
+    id: '2',
+    title: 'Notícia Urgente',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    photoCover:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW3X2pZ-GOOXBiN3GIE28uPOKTDwTYsmekgg&usqp=CAU',
+  },
+  {
+    id: '3',
+    title: 'Notícia Urgente 2',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    photoCover:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhup1oa6_3guy9ZOnU6XplqtvkAT06ZCYxUA&usqp=CAU',
+  },
+   {
+   id: '5',
+    title: 'Notícia Urgente 3',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    photoCover:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGKEUf4QrcowkUE929mW6K14cb8j5cwep5bA&usqp=CAU',
+  },
+   {
+   id: '6',
+    title: 'Notícia Urgente 4',
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    photoCover:
+      'https://quadrinheiros.files.wordpress.com/2019/02/marvel-comics.jpg',
+  }
+];
